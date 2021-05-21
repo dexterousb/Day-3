8633202b8165cb6e117134925b168ffdbb5babc0
dexterousb <84274083+dexterousb@users.noreply.github.com>
2021-05-21 12:01:33 +0530

    Add files via upload

---
+++ EvOd.py
@@ -0,0 +1,7 @@
+# find even and odd of number
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print("number dicides evenly by 2 so even ")
+else:
+    print("number doesnt divide evenly so odd ")
