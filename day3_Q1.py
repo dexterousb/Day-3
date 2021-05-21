17a84f0842aa3645088a9dc61aff3989d677e44f
dexterousb <84274083+dexterousb@users.noreply.github.com>
2021-05-21 11:59:10 +0530

    Add files via upload

---
+++ day3_Q1.py
@@ -0,0 +1,14 @@
+# WAP to sum of tuple elements 
+
+  
+# initializing tup 
+tup1 = (7, 8, 9, 1, 10, 7) 
+  
+# printing original tuple
+print("The original tuple is : " + str(tup1)) 
+  
+# sum function
+res = sum(list(tup1))
+  
+# printing result 
+print("The summation of tuple elements are : " + str(res)) 
